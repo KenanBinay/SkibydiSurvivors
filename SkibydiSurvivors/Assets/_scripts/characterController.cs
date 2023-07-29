a1733da52be5c4c53741c2810a60e17fa14939a2
Kenan <kenanbinay226@gmail.com>
2023-07-29 21:37:46 +0300

    enemy auto locking system created, character anim changes

--- SkibydiSurvivors/Assets/_scripts/characterController.cs
+++ SkibydiSurvivors/Assets/_scripts/characterController.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.Mathematics;
 using UnityEngine;
 
 public class characterController : MonoBehaviour
@@ -7,9 +8,10 @@ public class characterController : MonoBehaviour
     [SerializeField] FixedJoystick joystick;
 
     public CharacterController controller;
-    public GameObject R_hand, L_hand;
+    public GameObject R_hand, L_hand, enemy, fovStartPoint;
     public Animator playerAnimator;
-    public float movementSpeed, rotationSpeed;
+
+    public float movementSpeed, rotationSpeed, maxAngle = 90;
 
     void Start()
     {
@@ -19,13 +21,15 @@ public class characterController : MonoBehaviour
     void Update()
     {
         var movementDirection = new Vector3(joystick.Direction.x, 0.0f, joystick.Direction.y);
+
         controller.SimpleMove(movementDirection * movementSpeed);
 
         playerAnimator.SetFloat("vertical", movementDirection.sqrMagnitude);
 
         var targetDirection = Vector3.RotateTowards(controller.transform.forward, movementDirection
-        , rotationSpeed * Time.deltaTime, 0.0f);
+  , rotationSpeed * Time.deltaTime, 0.0f);
 
         controller.transform.rotation = Quaternion.LookRotation(targetDirection);
+
     }
 }
